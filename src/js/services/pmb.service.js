737dbe70337a74738b4660462d8c7a71af543b24
yonsonsal <ignacio.talavera@gmail.com>
2015-10-05 19:47:55 -0300

    Archivos Nuevos de services

---
+++ src/js/services/pmb.service.js
@@ -0,0 +1,35 @@
+starter.services.factory('PMBService', ['$http', 'leafletData', '$cordovaFileTransfer', function($http, leafletData, $cordovaFileTransfer) {
+  var base = "http://10.191.0.16:3000";
+
+  var PMBService = {
+
+    report: function(form) {
+
+      var promise;
+      if (form.file) {
+        var options ={};
+      return  $cordovaFileTransfer.upload(base + 'report/new/mobile', form.file, options)
+          .then(function(result) {
+            // Success!
+            //creo el reclamo
+            $http.post(base + 'report/new/mobile', form);
+          }, function(err) {
+            console.log("Error al subir el archivo",err);
+          }, function(progress) {
+            $timeout(function() {
+              $scope.downloadProgress = (progress.loaded / progress.total) * 100;
+            });
+          });
+
+      }else{
+        return $http.post(base + 'report/new/mobile', form);
+      }
+
+      //
+
+    }
+
+  };
+  return PMBService;
+
+}]);
---
+++ src/js/services/report.service.js
@@ -0,0 +1,49 @@
+starter.services.factory('ReportService', ['$http', 'leafletData', '$cordovaFileTransfer', function($http, leafletData, $cordovaFileTransfer) {
+  var base = "http://10.191.0.16:3000";
+
+
+  /**
+   * Constructor, with class name
+   */
+  function Report(_data) {
+    angular.extend(this, _data);
+  }
+
+Report._default = function(){
+  var _data = {
+    lat: 0,
+    lon: 0,
+    title: '',
+    details: '',
+    may_show_name: '',
+    category: '',
+    phone: '',
+    pc: '',
+    file: ''
+  };
+  return new Report(_data);
+};
+  Report._all = [];
+  Report.current = {};
+  Report._new = function(){
+    Report.current = Report._default();
+    return Report.current;
+  };
+
+  /**
+   * Static method, assigned to class
+   * Instance ('this') is not available in static context
+   */
+  Report.build = function(_data) {
+
+    return new Report(
+      _data
+    );
+  };
+
+  /**
+   * Return the constructor function
+   */
+  return Report;
+
+}]);
